9cd8210845829f66466346254a2be369793bf345
wassimfawaz45 <wassim.fawaz.45@gmail.com>
2026-04-01 16:39:08 +0300

    Create board.h

---
+++ include/board.h
@@ -0,0 +1,23 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#define BOX_ROWS 4
+#define BOX_COLS 5
+#define DOT_ROWS (BOX_ROWS + 1)
+#define DOT_COLS (BOX_COLS + 1)
+
+typedef struct {
+    int h[DOT_ROWS][BOX_COLS];
+    int v[BOX_ROWS][DOT_COLS];
+    char owner[BOX_ROWS][BOX_COLS];
+    int scoreA;
+    int scoreB;
+} Board;
+
+void board_init(Board *b);
+void board_print(const Board *b);
+int board_apply_move(Board *b, int player, int r1, int c1, int r2, int c2);
+int board_full(const Board *b);
+char board_winner(const Board *b);
+
+#endif
